e8967bcf59b15428d3aae13c6306f84bea3e6d6d
paweena.j <ik_kat@hotmail.co.th>
2019-11-08 17:48:54 +0700

    commit 2019-11-08

--- src/main/java/com/example/SpringTest/controllers/Aa.java
+++ src/main/java/com/example/SpringTest/controllers/Aa.java
@@ -1,8 +1,11 @@
 package com.example.SpringTest.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -12,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * http://localhost:8080/api/hello
  */
-@RestController
+/*@RestController
 //@RequestMapping ("/api/example")
 @RequestMapping ("/api")
 public class Aa {
@@ -22,5 +25,15 @@ public class Aa {
   public ResponseEntity<String> get() {
     return ResponseEntity.ok("Hello World!  by  [[ KAT ]]");
   }
-
+}*/
+@Controller
+public class Aa {
+  @GetMapping({"/", "/index"})
+  public String showPage(){
+    return "index";
+  }
+  /*public String hello(Model model, @RequestParam(value="name", required=false, defaultValue="KAT") String name) {
+    model.addAttribute("name", name);
+    return "index";
+  }*/
 }
